af956e3824511d49399aac3290edcbdb09048451
SUTTHIDA JOIROONG <sutthida.joi04@gmail.com>
2025-05-18 14:36:11 +0700

    Update RotateLayersInGroup.jsx

--- RotateLayersInGroup.jsx
+++ RotateLayersInGroup.jsx
@@ -4,7 +4,7 @@ function rotateLayersInGroup(groupName, angle) {
     var doc = app.activeDocument;
     var theGroup = null;
 
-    // ค้นหา Group ที่ชื่อว่า groupName
+
     for (var i = 0; i < doc.layerSets.length; i++) {
         if (doc.layerSets[i].name == groupName) {
             theGroup = doc.layerSets[i];
@@ -17,7 +17,6 @@ function rotateLayersInGroup(groupName, angle) {
         return;
     }
 
-    // วนลูปทุก Layer ในกลุ่ม แล้วหมุน
     for (var j = 0; j < theGroup.artLayers.length; j++) {
         var layer = theGroup.artLayers[j];
         doc.activeLayer = layer;
@@ -26,6 +25,6 @@ function rotateLayersInGroup(groupName, angle) {
 
 }
 
-// เรียกใช้ฟังก์ชัน หมุน Group 2 ทีละ Layer 90 องศา
+// using the function
 rotateLayersInGroup("Group 3", -90);
 rotateLayersInGroup("Group 4", 180);
